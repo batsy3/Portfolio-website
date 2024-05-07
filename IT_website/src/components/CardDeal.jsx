3b7f7de755bb5d0d0c663f3f5f55639e9f9ac367
D!3 4 UZ! <dvdokuku@gmail.com>
2024-05-07 18:37:11 +0200

    Navbar fixees (#1)
    * navbar setup
    
    * body setup
    
    * billing and card deal update
    
    * card deal section
    
    * nav bar fix

--- IT_website/src/components/CardDeal.jsx
+++ IT_website/src/components/CardDeal.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import React from "react";
+import styles, { layout } from "../style";
+import Button from "./Button";
+import { card } from "../assets";
 const CardDeal = () => {
   return (
-    <div>CardDeal</div>
-  )
-}
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          FInd a better Card deal <br className="sm:block hidden" />
+          in few easy steps!
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed
+          lorem ac dui fermentum dictum a non leo. Vivamus augue lorem,
+        </p>
+        <Button styles="mt-10"></Button>
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card-deal" className=""/>
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] rounded-full white__gradient" />
+        <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] rounded-full pink__gradient" />
+      </div>
+    </section>
+  );
+};
 
-export default CardDeal
+export default CardDeal;
--- IT_website/src/components/Hero.jsx
+++ IT_website/src/components/Hero.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
+import styles from "../style";
+import { discount, robot } from "../assets";
+import GetStarted from "./GetStarted";
+const Hero = () => (
+  <section id="Home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+    <div
+      className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
+    >
+      <div
+        className={`flex flex-row items py-[6px] px-[4px] bg-discoun-gradient rounded-[10px] mb-2`}
+      >
+        <img src={discount} alt="discount" className="w-[32px] h[32px]" />
+        <p className={`${styles.paragraph} ml-2`}>
+          <span className="text-white ">20%</span> Discount For
+          <span className="text-white "> 1 Month</span> Account
+        </p>
+      </div>
+      <div className="flex flex-row justify-between items-center w-full">
+        <h1
+          className={`  flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]`}
+        >
+          The Next <br className="sm:block hidden" />{" "}
+          <span className="text-gradient">Generation </span>
+        </h1>
+        <div className="ss:flex hidden md:mr-4 mr-0">
+          <GetStarted />
+        </div>
+      </div>
+      <h1
+        className={`  flex-1 font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full`}
+      >
+        Payment Method
+      </h1>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed
+        lorem ac dui fermentum dictum a non leo. Vivamus augue lorem, facilisis
+        non fringilla vitae, aliquet molestie justo. Mauris finibus ipsum eu
+        libero vulputate finibus vitae blandit turpis.{" "}
+      </p>
+    </div>
+    <div
+      className={`flex-1 ${styles.flexCenter} md:my-0 my-10 relative`}
+    >
+      <img
+        className="w-[100%] h-[100%] relative z-[5]"
+        src={robot}
+        alt="billing"
+      />
+      <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+      <div className="absolute z-[1] w-[80%] h-[80%] top-0 white__gradient" />
+      <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 top-0 blue__gradient" />
+    </div>
+    <div className={`ss:hidden ${styles.flexCenter}`}>
+      <GetStarted />
+    </div>
+  </section>
+);
 
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
-
-export default Hero
+export default Hero;
